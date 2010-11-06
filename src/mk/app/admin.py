f4fb6009720c273b0230bc9ab7a635d37a1b5891
Martin <martin.laine@gmail.com>
2010-11-06 01:30:29 +0000

    Model work

--- src/mk/app/admin.py
+++ src/mk/app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from mk.app.models import Player, Course
+from mk.app.models import Player, Course, Event, Race
 
 admin.site.register(Player)
 
 admin.site.register(Course)
+
+admin.site.register(Event)
+
+admin.site.register(Race)
--- src/mk/app/models.py
+++ src/mk/app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+POSITION_POINTS = {
+	'firsts': 15,
+	'seconds': 9,
+	'thirds': 4,
+	'fourths': 1,
+}
+
+
 class Player(models.Model):
 	class Meta:
 		ordering = ['name']
 	
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, unique=True)
 	
 	def __unicode__(self):
 		return self.name
@@ -13,7 +21,31 @@ class Course(models.Model):
 	class Meta:
 		ordering = ['name']
 	
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, unique=True)
 	
 	def __unicode__(self):
 		return self.name
+	
+class Event(models.Model):
+	class Meta:
+		ordering = ['event_date']
+		
+	event_date = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	players = models.ManyToManyRel(Player)
+	
+	def __unicode__(self):
+		return unicode(self.event_date)
+	
+class Race(models.Model):
+	event = models.ForeignKey(Event)
+	course = models.ForeignKey(Course)
+	order = models.IntegerField()
+	first = models.ForeignKey(Player, related_name='firsts')
+	second = models.ForeignKey(Player, related_name='seconds')
+	third = models.ForeignKey(Player, related_name='thirds')
+	fourth = models.ForeignKey(Player, related_name='fourths')
+	
+	def __unicode__(self):
+		return unicode(self.event.event_date) + " - " + self.course.name
+	
