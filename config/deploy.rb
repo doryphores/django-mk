7eab6ad1e1baf687abf987285ca0c907a2fbda63
Martin Laine <martin.laine@gmail.com>
2011-01-20 18:17:37 +0000

    Final tweaks to cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ set :domain, "192.168.1.134"
 
 set :deploy_to, "/home/martin/www/django-mk-prod"
 set :db_location, "src"
-set :app_location, "src/mk"
-set :apache_connector, "#{app_location}/apache/connector.wsgi"
+set :project_location, "src/mk"
+set :apache_connector, "#{project_location}/apache/connector.wsgi"
 set :static_location, "src/static"
 set :django_admin_media, "/usr/local/lib/python2.6/dist-packages/django/contrib/admin/media"
 set :backup_dir, "#{deploy_to}/backups"
@@ -29,6 +29,11 @@ role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
 
+
+after "deploy:update", "deploy:cleanup"
+
+# Backups
+
 namespace :backup do
   task :setup do
     run "mkdir -p #{deploy_to}/#{backup_dir}"
@@ -41,6 +46,9 @@ namespace :backup do
   after   "deploy:setup", "backup:setup"
   before  "deploy", "backup:db"
 end
+
+
+# Shared folders
 
 namespace :shared do
   desc <<-EOD
@@ -76,6 +84,9 @@ namespace :shared do
   on :start,  "shared:register_dirs"
 end
 
+
+# Override default capistrano tasks
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -83,30 +94,40 @@ namespace :deploy do
     run "chown -R :www-data #{latest_release}"
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
     
+    # Symlink to admin media folder
     run "ln -s #{django_admin_media} #{latest_release}/#{static_location}/admin_media"
-    run "ln -s #{shared_path}/system/local_settings.py #{latest_release}/#{app_location}/local_settings.py"
+    
+    # Symlink database
     run "ln -s #{shared_path}/system/db.sqlite #{latest_release}/#{db_location}/db.sqlite"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
+    # Touch WSGI script to restart app
     run "#{try_sudo} touch #{current_path}/#{apache_connector}"
   end
+end
+
+
+# Django specific tasks
+
+def django_manage(cmd, options={})
+  run "cd #{latest_release}/#{project_location}; python manage.py #{cmd}", options
 end
 
 namespace :django do
   desc "Perform db migrations"
   task :migrate do
-    run "#{current_path}/#{app_location}/manage.py syncdb --noinput --migrate"
+    django_manage "syncdb --noinput --migrate"
   end
   
   desc "Re-run history"
   task :update do
-    run "#{current_path}/#{app_location}/manage.py update_history --noinput"
+    django_manage "update_history --noinput"
   end
   
   desc "Cleanup"
   task :cleanup do
-    run "#{current_path}/#{app_location}/manage.py cleanup"
+    django_manage "cleanup"
   end
   
-  after "deploy", "django:migrate"
+  after "deploy", "django:migrate", "django:cleanup", "django:update"
 end
