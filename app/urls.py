bba9779bf544ab07b1faf05219686356f3bc0205
Martin <martin.laine@gmail.com>
2011-08-22 15:08:52 +0100

    Simpified URL configuration and switched to shortcuts

--- app/urls.py
+++ app/urls.py
@@ -2,15 +2,21 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('app.views',
 	url(r'^$', 'home', name="home-page"),
+	
+	# Race recorder
 	url(r'^new/$', 'new', name="new-event"),
-	(r'^race/(?P<race_id>\d+)/$', 'race'),
-	(r'^race/$', 'race'),
+	url(r'^race/(?P<race_id>\d+)/$', 'race', name="race"),
+	url(r'^race/$', 'race', name="race-start"),
 	url(r'^confirm/$', 'confirm', name="confirm-results"),
 	url(r'^finish/$', 'finish', name="finish-event"),
-	url(r'^players/$', 'players', name='player-list'),
-	(r'^player/(?P<player_id>\d+)/$', 'player'),
-	url(r'^player/(?P<player_id>\d+)/events/$', 'player_events', name="player-events"),
-	url(r'^player/(?P<player_id>\d+)/tracks/$', 'player_tracks', name="player-tracks"),
+	
+	# Players section
+	url(r'^players/$', 'players', name="player-list"),
+	url(r'^players/(?P<player_id>\d+)/$', 'player', name="player-details"),
+	url(r'^players/(?P<player_id>\d+)/events/$', 'player_events', name="player-events"),
+	url(r'^players/(?P<player_id>\d+)/tracks/$', 'player_tracks', name="player-tracks"),
+	
+	# Tracks section
 	url(r'^tracks/$', 'tracks', name="track-list"),
-	(r'^track/(?P<track_id>\d+)/$', 'track'),
+	url(r'^tracks/(?P<track_id>\d+)/$', 'track', name="track-details"),
 )
