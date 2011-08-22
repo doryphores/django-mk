b7bfebd59d366a102fd1716d777d367719a0b2f0
Martin <martin.laine@gmail.com>
2011-08-22 16:31:56 +0100

    Fixed test using sessions (Added session app)

--- settings.py
+++ settings.py
@@ -114,6 +114,7 @@ INSTALLED_APPS = (
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sites',
+	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.admin',
 	'django.contrib.staticfiles',
